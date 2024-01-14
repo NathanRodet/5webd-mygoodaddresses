5f6e6d6207f6239b519bec9b2219746c59ae1c55
Nathan Rodet <rodetnathan.contact@gmail.com>
2024-01-14 14:59:45 +0100

    initial commit

---
+++ MyGoodAddresses/utils/firebase.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import fireBaseConfig from "../firebase.config.json"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Type for the configuration object
+type FirebaseConfigType = {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+
+// Configuration as a typed object
+const firebaseConfig: FirebaseConfigType = fireBaseConfig as FirebaseConfigType;
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
