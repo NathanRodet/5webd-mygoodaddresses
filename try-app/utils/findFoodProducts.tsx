c160566470565feaf57e3777eacd9109258f846b
Nathan Rodet <rodetnathan.contact@gmail.com>
2024-01-16 21:55:51 +0100

    Removed old project artefact

--- try-app/utils/findFoodProducts.tsx
+++
@@ -1,8 +0,0 @@
-import { FoodProducts } from "../models/FoodProducts";
-import foodProductsJson from '../data/foodproducts.json';
-
-export function findFoodProducts(): FoodProducts {
-  const foodProducts = foodProductsJson as FoodProducts;
-
-  return foodProducts;
-}
